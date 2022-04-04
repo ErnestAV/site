c7351cd282bbe77367f26ab4b0ad130121bd0d63
ErnestAV <59626561+ErnestAV@users.noreply.github.com>
2022-04-04 04:03:57 -0600

    Site deployed

---
+++ build/precache-manifest.c5f91a0647c51935f100b5c277f068c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63be8a314c186d86f838c880b249a12f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb564f7311b0ce01dc73",
+    "url": "/static/css/main.148f96f6.chunk.css"
+  },
+  {
+    "revision": "302b0d32bb224d7b027e",
+    "url": "/static/js/2.d224a1e4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d224a1e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb564f7311b0ce01dc73",
+    "url": "/static/js/main.e126215f.chunk.js"
+  },
+  {
+    "revision": "6fc4f2c3fd5ae8d05fd1",
+    "url": "/static/js/runtime-main.4ba1596e.js"
+  }
+]);
