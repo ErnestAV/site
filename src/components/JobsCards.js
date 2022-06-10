e8c5bd9639a8511ceacd3749d58fe6f937a60e7e
ErnestAV <59626561+ErnestAV@users.noreply.github.com>
2022-06-10 00:32:16 -0700

    Updating logos and minor CSS changes

--- src/components/JobsCards.js
+++ src/components/JobsCards.js
@@ -11,13 +11,13 @@ function JobsCards() {
           <ul className='cards__items'>
             <CardItem
               href='https://www.dish.com/'
-              src='images/Dish_logo.png'
-              text='DISH Network'
+              src='images/Dish-sling.jpeg'
+              text='DISH Network (Sling)'
               label='Associate Software Engineer'
             />
             <CardItem
               href='https://amazon.jobs/en/teams/lab126/'
-              src='images/Lab126logo.avif'
+              src='images/AmazonLab126_logo.avif'
               text='Amazon Lab126'
               label='Software Development Engineer Intern'
             />
@@ -31,13 +31,13 @@ function JobsCards() {
             />
             <CardItem
               href='https://cs.byu.edu/'
-              src='images/byu_logo.png'
+              src='images/byu_off.png'
               text='BYU Computer Science'
               label='Research/Teaching Assistant'
             />
             <CardItem
               href='https://applaudostudios.com/'
-              src='images/applaudoStudios_logo.jpeg'
+              src='images/applaudoStudios_logo2.png'
               text='Applaudo Studios'
               label='Software Engineer Trainee'
             />
